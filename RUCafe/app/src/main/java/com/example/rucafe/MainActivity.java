ba9abcdf924b3749b5df13872738b7bc80a96564
halimasadiq <67449331+halimasadiq@users.noreply.github.com>
2022-04-21 22:04:57 -0400

    icon created

--- RUCafe/app/src/main/java/com/example/rucafe/MainActivity.java
+++ RUCafe/app/src/main/java/com/example/rucafe/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openDonuts(View view){
+        Intent intent = new Intent(MainActivity.this, DonutActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCoffee(View view){
+        Intent intent = new Intent(MainActivity.this, CoffeeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCart(View view){
+        Intent intent = new Intent(MainActivity.this, BasketActivity.class);
+        startActivity(intent);
+    }
+
+    public void openOrders(View view){
+        Intent intent = new Intent(MainActivity.this, StoreOrdersActivity.class);
+        startActivity(intent);
     }
 }
